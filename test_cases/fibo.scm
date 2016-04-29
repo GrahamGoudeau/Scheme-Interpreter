525baf2c3ab67da4c999da24437ffbf0ee10c335
Graham Goudeau <grahamgoudeau@gmail.com>
2016-04-29 17:12:42 -0400

    .gitignore

---
+++ test_cases/fibo.scm
@@ -0,0 +1,12 @@
+(define fibo (n)
+  (if (< n 1) 
+    0
+    (if (< n 2)
+      1
+      (+ (fibo (- n 1)) (fibo (- n 2))))))
+
+(check-expect (fibo 0) 0)
+(check-expect (fibo 1) 1)
+(check-expect (fibo 2) 1)
+(check-expect (fibo 3) 2)
+(check-expect (fibo 12) 144)
---
+++ test_cases/recursion.scm
@@ -0,0 +1,14 @@
+(define find-divisor (target n)
+  (if (= target n)
+    target
+    (if (= (mod target n) 0)
+      n
+      (find-divisor target (+ n 1)))))
+
+(define prime (n)
+  (or (= n 2) (= n (find-divisor n 2))))
+
+(check-expect (find-divisor 3 2) 3)
+(check-expect (prime 2) #t)
+(check-expect (prime 3) #t)
+(check-expect (prime 4) #f)
